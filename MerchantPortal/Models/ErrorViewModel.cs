12d9de5ac4f1c3de1a6834cff107c670265e4dc4
osmanaliaydemir <aydemirosmanali@gmail.com>
2026-01-04 14:07:46 +0300

    chore: Remove the entire MerchantPortal application and its related files.

--- MerchantPortal/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Getir.MerchantPortal.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
--- MerchantPortal/Services/ApiSettings.cs
+++
@@ -1,8 +0,0 @@
-namespace Getir.MerchantPortal.Services;
-
-public class ApiSettings
-{
-    public string BaseUrl { get; set; } = default!;
-    public string SignalRHubUrl { get; set; } = default!;
-}
-
